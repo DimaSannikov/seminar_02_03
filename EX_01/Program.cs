da86dd7d3b45af55452155fa1dd0797879905faf
DimaSannikov <dimasannikov@gmail.com>
2023-03-03 11:17:34 +0500

    changed EX_01

--- EX_01/Program.cs
+++ EX_01/Program.cs
@@ -1,8 +1,22 @@
-﻿for(int i = 0; i < 100; i++)
+﻿// Цикл for
+
+for(int i = 0; i < 100; i++)
 {
     if(i == 50)
     {
         break;
     }
-    Console.WriteLine($"{i} Hello world");
+    // Console.WriteLine($"{i} Hello world");
 }
+
+
+// Методы
+
+int GetSum(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+
+int sum = GetSum(10, 15);
+Console.WriteLine(sum);
