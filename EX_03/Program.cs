ab3f30d9a8a8f3001d710618743cf344d5275b22
DimaSannikov <dimasannikov@gmail.com>
2023-03-03 11:18:01 +0500

    added EX_03

---
+++ EX_03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+Console.Write("Ведите X1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ведите Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ведите X2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Ведите Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+
+double get2D(int xa, int ya, int xb, int yb)
+{
+    double dist = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+    return dist;
+}
+
+
+double distance = Math.Round(get2D(x1, y1, x2, y2), 3, MidpointRounding.ToEven);
+Console.WriteLine(distance);
