807e737c1437bc12bf5be6936610e4082b937411
DimaSannikov <dimasannikov@gmail.com>
2023-03-03 11:17:51 +0500

    added EX_02

---
+++ EX_02/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Ведите номер четверти от 1 до 4: ");
+int digit = int.Parse(Console.ReadLine());
+
+
+void returnQuart(int chet)
+{
+    switch(chet)
+    {
+        case 1:
+            Console.WriteLine("X > 0; Y > 0");
+            break;
+        
+        case 2:
+            Console.WriteLine("X < 0; Y > 0");
+            break;
+        
+        case 3:
+            Console.WriteLine("X < 0; Y < 0");
+            break;
+        
+        case 4:
+            Console.WriteLine("X > 0; Y < 0");
+            break;
+        
+        default:
+            Console.WriteLine("Такой четверти нет");
+            break;
+    }
+}
+
+
+returnQuart(digit);
