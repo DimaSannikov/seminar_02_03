7199ad943c3b934e5788ee1b85d2754892a368ec
DimaSannikov <dimasannikov@gmail.com>
2023-03-03 13:21:46 +0500

    added HW_03

---
+++ HW_03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число
+// (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Ведите целое число: ");
+int digit = int.Parse(Console.ReadLine());
+
+
+void getSqr(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        double sqr = Math.Pow(i, 3);
+        Console.WriteLine(sqr);
+    }
+}
+
+
+getSqr(digit);
