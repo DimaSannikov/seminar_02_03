4e34e5aa5a85b0d6cbfdbf137a4d3e828cdb9ba5
DimaSannikov <dimasannikov@gmail.com>
2023-03-03 11:18:08 +0500

    added EX_04

---
+++ EX_04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+// Console.Write("Ведите целое число: ");
+// int digit = int.Parse(Console.ReadLine());
+
+// for(int i = 1; i <= digit; i++)
+// {
+//     double sqr = Math.Pow(i, 2);
+//     Console.WriteLine(sqr);
+// }
+
+Console.Write("Ведите целое число: ");
+int digit = int.Parse(Console.ReadLine());
+
+
+void getSqr(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        double sqr = Math.Pow(i, 2);
+        Console.WriteLine(sqr);
+    }
+}
+
+
+getSqr(digit);
